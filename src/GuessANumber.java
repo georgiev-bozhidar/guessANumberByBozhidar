b963cf464fe8d5bed59cb7be4fa670290d15777d
Exdia <124601724+georgiev-bozhidar@users.noreply.github.com>
2023-02-08 15:00:43 +0200

    Added user input evaluation
    Added input evaluation to make sure that the input given by the user is a number

--- src/GuessANumber.java
+++ src/GuessANumber.java
@@ -1,29 +1,67 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/*
+Guess A Number Game
+
+TODO: Make game run in a console window
+TODO: Make a text speed function
+TODO: Add difficulty
+    - make the range custom
+TODO: Add game restart
+    - once the game is over, ask the player if they want to go back to the main menu or restart the game
+TODO: Add story mode
+TODO: Add arcade mode
+TODO: Add custom game
+TODO: Add options (text speed,
+TODO: Add quit button to main menu
+
+ */
+
 public class GuessANumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Random rng = new Random();
-        int generatedNumber = rng.nextInt(100);
+        while(true){
+            Random rng = new Random();
+            int generatedNumber = rng.nextInt(100);
 
-        int input = takeUserInput();
+            String userInput = scanner.nextLine();
+            boolean isValidInput = evaluateStringToInt(userInput);
+            int input;
+            if (isValidInput) input = Integer.parseInt(userInput);
+            else {
+                System.out.println("Invalid input!");
+                continue;
+            }
 
-        boolean isCorrectGuess = checkNumber(input, generatedNumber);
-        while (!isCorrectGuess){
-            input = takeUserInput();
-            isCorrectGuess = checkNumber(input, generatedNumber);
+            boolean isCorrectGuess = checkNumber(input, generatedNumber);
+            while (!isCorrectGuess){
+                userInput = scanner.nextLine();
+                isValidInput = evaluateStringToInt(userInput);
+                if (isValidInput) input = Integer.parseInt(userInput);
+                else System.out.println("Invalid input!");
+                isCorrectGuess = checkNumber(input, generatedNumber);
+            }
+
+            System.out.println("You guessed it!");
+
         }
-
-        System.out.println("You guessed it!");
     }
 
-    public static int takeUserInput(){
-        System.out.print("Guess the number (1-100): ");
-        Scanner scanner = new Scanner(System.in);
-        return Integer.parseInt(scanner.nextLine());
+    public static boolean evaluateStringToInt(String string){
+        char[] stringToCharArr = string.toCharArray();
+        for (char ch : stringToCharArr){
+            if (!Character.isDigit(ch)) return false;
+        }
+        return true;
     }
+
+//    public static int takeUserInput(){
+//        System.out.print("Guess the number (1-100): ");
+//        Scanner scanner = new Scanner(System.in);
+//        return Integer.parseInt(scanner.nextLine());
+//    }
 
     public static boolean checkNumber(int n, int generatedNumber){
         if (n > generatedNumber) {
